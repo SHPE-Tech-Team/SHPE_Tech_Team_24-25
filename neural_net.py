a6d19a5ad8bd53b4463c5577fd3a238fcbeb9b1d
Kevin <kcruz28@illinois.edu>
2024-12-18 01:30:01 -0600

    sheesh

--- neural_net.py
+++ neural_net.py
@@ -49,13 +49,11 @@ class Network(nn.Module):
 
 train_transform = transforms.Compose(
     [
-        transforms.Resize(256),  
-        transforms.RandomCrop(224),  
-        transforms.RandomHorizontalFlip(), 
-        transforms.RandomRotation(degrees=15), 
-        transforms.ColorJitter(
-            brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1
-        ),  # Adjust colors
+        transforms.Resize(256),
+        transforms.RandomCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(degrees=15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
