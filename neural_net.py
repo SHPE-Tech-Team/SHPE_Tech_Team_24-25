41f1eec58f09231490a268a97da8d3191f6cce34
Kevin <kcruz28@illinois.edu>
2024-12-18 01:24:03 -0600

    transform function

--- neural_net.py
+++ neural_net.py
@@ -45,3 +45,27 @@ class Network(nn.Module):
         pooled_features = self.avgpool(features)
         output = self.classifier(torch.flatten(pooled_features, 1))
         return output
+
+
+train_transform = transforms.Compose(
+    [
+        transforms.Resize(256),  
+        transforms.RandomCrop(224),  
+        transforms.RandomHorizontalFlip(), 
+        transforms.RandomRotation(degrees=15), 
+        transforms.ColorJitter(
+            brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1
+        ),  # Adjust colors
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+
+test_transform = transforms.Compose(
+    [
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
