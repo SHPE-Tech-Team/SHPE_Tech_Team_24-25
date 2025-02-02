1ee37bed30d34227a0c78289b7b7667a648a79b9
Kevin <kcruz28@illinois.edu>
2025-02-02 14:52:24 -0600

    DONENEENE

--- back/flask_server/server.py
+++ back/flask_server/server.py
@@ -59,5 +59,5 @@ def handle_connect():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, host="0.0.0.0", port=8080)
-    socketio.run(app, debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
+    # socketio.run(app, debug=True, host="0.0.0.0", port=8080)
