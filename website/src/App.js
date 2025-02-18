a30608eb3b346301aae1a7d2aa3af39fc69027b4
atino2 <atino2@illinois.edu>
2025-02-17 18:43:51 -0600

    add picture

--- website/src/App.js
+++ website/src/App.js
@@ -1,17 +1,28 @@
 import React,{ useState, useEffect }from "react";
 import ObjectDetection from './camera';
 import RealtimePrediction from './data_predi';
+import myImage from "./dlt.png";
+
+
+
+function MyComponent() {
+  return <img src={myImage} alt = "3D Printing" width = '500' height = '400' />;
+}
 
 function App() {
   return (
     <div>
-      <h1>AI LOTERIA - LA FLAME🔥</h1>
-      <h1>Alonso is a better programmer than Kevin</h1>
+      <div style = {{textAlign: 'center'}}>
+        <h1>SHPE TECH TEAM - AI Loteria</h1>
+        <MyComponent />
+      </div>
 
+      
       <ObjectDetection />
       <RealtimePrediction /> 
     </div>
   );
 }
+
 
 export default App;
