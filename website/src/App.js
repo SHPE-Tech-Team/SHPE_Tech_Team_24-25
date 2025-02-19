7394a32036d20b5605e4f455ae3d238a1924801c
atino2 <atino2@illinois.edu>
2025-02-18 23:21:15 -0600

    added video

--- website/src/App.js
+++ website/src/App.js
@@ -2,11 +2,20 @@ import React,{ useState, useEffect }from "react";
 import ObjectDetection from './camera';
 import RealtimePrediction from './data_predi';
 import myImage from "./dlt.png";
+import myVid from "./print.mp4";
 
 
 
 function MyComponent() {
   return <img src={myImage} alt = "3D Printing" width = '500' height = '400' />;
+}
+
+function Myvideo() {
+  return (
+  <video width = "750" height = "500" controls>
+    <source src={myVid} type = "video/mp4"/>
+  </video>
+  );
 }
 
 function App() {
@@ -15,6 +24,7 @@ function App() {
       <div style = {{textAlign: 'center'}}>
         <h1>SHPE TECH TEAM - AI Loteria</h1>
         <MyComponent />
+        <Myvideo />
       </div>
 
       
