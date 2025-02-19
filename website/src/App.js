e8f14f6735ac4bcf3e97f77043ac0a28f1d2ae4a
atino2 <atino2@illinois.edu>
2025-02-19 11:21:26 -0600

    changed background color, font color, and font size

--- website/src/App.js
+++ website/src/App.js
@@ -5,7 +5,6 @@ import myImage from "./loteria.png";
 import myVid from "./print.mp4";
 
 
-
 function MyComponent() {
   return <img src={myImage} alt = "3D Printing" width = '900' height = '500' />;
 }
@@ -20,15 +19,15 @@ function Myvideo() {
 
 function App() {
   return (
-      <div style = {{textAlign: 'center'}}>
-        <h1>SHPE TECH TEAM - AI Loteria</h1>
+      <div style = {{textAlign: 'center', backgroundColor: '#041E42', color: 'white', minHeight: '100vh', padding: '20px'}}>
+        <h1 style = {{fontSize: '50px'}}>SHPE TECH TEAM - AI Loteria</h1>
 
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
           <MyComponent />
+          <h1>This project brings a new approach to the popular Latin American game Lotería. With using Python libraries that utilize convolutional neural networks and deep learning, Lotería can become fully automized.</h1>
           <Myvideo />
       </div>
 
-      
       <ObjectDetection />
       <RealtimePrediction /> 
     </div>
