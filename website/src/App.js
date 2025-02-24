24e654170c69e322fe6d0a56afb3ab45752b85a8
Kevin <kcruz28@illinois.edu>
2025-02-23 20:42:10 -0600

    fixed router

--- website/src/App.js
+++ website/src/App.js
@@ -12,10 +12,6 @@ import MyHand from "./hand_project.js";
 import MyAI from "./ai_loteria.js"
 
 
-function MyComponent() {
-  return <img src="https://www.google.com/logos/doodles/2019/celebrating-loteria-6753651837108226.3-2xa.gif" alt = "3D Printing" width = '900' height = '500' />;
-}
-
 function Myvideo() {
   return (
   <video width = "750" height = "500" controls>
@@ -47,8 +43,11 @@ function App() {
      <Navbar></Navbar>
        <div style = {{textAlign: 'center'}}>
         
-         <h1>SHPE TECH TEAM - AI Loteria</h1>
-         <MyComponent />
+         <h1>Engineering Open House 2025</h1>
+         <h3><em>The Age of Innovation</em></h3>
+
+         <h4>Project 1</h4>
+         <h2>AI Loteria</h2>
          <div className="video-container">
            <ObjectDetection />
            {/* <RealtimePrediction />  */}
@@ -56,6 +55,8 @@ function App() {
          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
           
            <Myvideo />
+
+           <h3>Made by SHPE UIUC Tech Team</h3>
          </div>      
      </div>
      </div>
