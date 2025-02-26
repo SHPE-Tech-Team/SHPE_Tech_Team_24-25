66e4819abc1ca56498470271363dac1f9c8376c4
Kevin <kcruz28@illinois.edu>
2025-02-26 03:28:52 -0600

    Did the routing and gave everyone tasks

---
+++ react_frontend/src/pages/contact_page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/// danny's pg to due
+/// Do figma concept before programming  !!!!!!!!!!!!
+
+function ContactPage() {
+  return (
+    <div>
+      <h1>Danny this is your page to do</h1>
+      <h1 style={{color: "red"}} ><em> Do figma concept before programming  !!!!!!!!!!!!</em></h1>
+    </div>
+  );
+}
+
+export default ContactPage;
