d229c36ff27bf8f08404faef95e616ed7a3b7223
Kevin <kcruz28@illinois.edu>
2025-03-19 00:51:26 -0500

    added content to about page

--- react_frontend/src/pages/about_page.jsx
+++ react_frontend/src/pages/about_page.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Footer from "../components/footer.jsx";
 import CardDeckPrediction from "./card_detection/card_deck_prediction.jsx";
 import TitleCard from "../components/title_card.jsx";
-// alonso pg to do
-/// Do figma concept before programming  !!!!!!!!!!!!
+import "../styles/about_style.css";
+
 function AboutPage() {
   return (
     <div>
@@ -11,6 +11,27 @@ function AboutPage() {
         title="About Us"
         description="Learn more about the Society of Hispanic Professional Engineers Tech Team at the University of Illinois at Urbana-Champaign."
       />
+
+      <div className="about-content">
+        <h2>Our Mission</h2>
+        <p>
+          The Society of Hispanic Professional Engineers is a technical
+          organization with a mission to empower the Hispanic community to
+          realize its fullest potential and to impact the world through STEM
+          awareness, access, support, and development.
+        </p>
+        <p>
+          SHPE's Technical Team is dedicated to providing members with valuable
+          technical experience while developing real-world engineering
+          solutions. Our team works on various projects that allow members to
+          apply classroom knowledge to practical challenges.
+        </p>
+        <p>
+          We strive to create an inclusive environment where Hispanic engineers
+          can grow professionally, develop leadership skills, and build a strong
+          network of peers and mentors in the engineering field.
+        </p>
+      </div>
       <Footer></Footer>
     </div>
   );
