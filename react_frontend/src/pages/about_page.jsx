3afdbbaf3c68756cae6fe1ea70ba59000983ebc4
Kevin <kcruz28@illinois.edu>
2025-03-19 00:22:09 -0500

    created title card

--- react_frontend/src/pages/about_page.jsx
+++ react_frontend/src/pages/about_page.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Footer from "../components/footer.jsx";
 import CardDeckPrediction from "./card_detection/card_deck_prediction.jsx";
+import TitleCard from "../components/title_card.jsx";
 // alonso pg to do
 /// Do figma concept before programming  !!!!!!!!!!!!
 function AboutPage() {
   return (
-    <div className="about">
-      <div className="about-background">
-        <div className="about-text">
-          <h1>About</h1>
-        </div>
-      </div>
+    <div>
+
       <Footer></Footer>
     </div>
   );
