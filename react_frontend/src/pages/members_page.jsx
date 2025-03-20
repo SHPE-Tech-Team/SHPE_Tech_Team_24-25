a1f079631058591148796b8728431be103d1d8f6
Kevin <kcruz28@illinois.edu>
2025-03-19 22:49:29 -0500

    added layout for members page

--- react_frontend/src/pages/members_page.jsx
+++ react_frontend/src/pages/members_page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Footer from "../components/footer.jsx";
 import TitleCard from "../components/title_card.jsx";
+import MemberCard from "../components/member_card";
+import "../styles/members_page_style.css";
 
 function MembersPage() {
   return (
@@ -9,6 +11,60 @@ function MembersPage() {
         title="Team Members"
         description="Meet the talented engineers behind SHPE Tech Team's innovative projects. Our diverse team brings together expertise in programming, mechanical  engineering, and more."
       />
+
+      <div className="team-section">
+        <h2>The Team</h2>
+        <div className="team-row">
+          <MemberCard
+            member={{
+              avatar: "/home_media/dlt.png",
+              name: "John Doe",
+              email: "john.doe@example.com",
+              role: "Developer",
+            }}
+          />
+          <MemberCard
+            member={{
+              avatar: "/home_media/dlt.png",
+              name: "La Flame",
+              email: "john.doe@example.com",
+              role: "Lead Software Developer",
+            }}
+          />
+          <MemberCard
+            member={{
+              avatar: "/home_media/dlt.png",
+              name: "John Doe",
+              email: "john.doe@example.com",
+              role: "Developer",
+            }}
+          />
+          <MemberCard
+            member={{
+              avatar: "/home_media/dlt.png",
+              name: "John Doe",
+              email: "john.doe@example.com",
+              role: "Developer",
+            }}
+          />
+          <MemberCard
+            member={{
+              avatar: "/home_media/dlt.png",
+              name: "La Flame",
+              email: "john.doe@example.com",
+              role: "Lead Software Developer",
+            }}
+          />
+          <MemberCard
+            member={{
+              avatar: "/home_media/dlt.png",
+              name: "John Doe",
+              email: "john.doe@example.com",
+              role: "Developer",
+            }}
+          />
+        </div>
+      </div>
       <Footer></Footer>
     </div>
   );
