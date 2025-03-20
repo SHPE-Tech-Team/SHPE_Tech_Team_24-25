91c84d1b91d2c43bae0a8fa8d8a4775305e22a54
Kevin <kcruz28@illinois.edu>
2025-03-20 03:08:57 -0500

    created toggle to seperate pro and mech

--- react_frontend/src/data/members_data.jsx
+++ react_frontend/src/data/members_data.jsx
@@ -1,4 +1,4 @@
-const members = [
+const programmingTeam = [
   {
     avatar: "/AI_Loteria_24-25/home_media/dlt.png",
     name: "Kevin Cruz (La Flame)",
@@ -43,6 +43,43 @@ const members = [
   },
 ];
 
+const mechanicalTeam = [
+  {
+    avatar: "/AI_Loteria_24-25/home_media/dlt.png",
+    name: "Juan Fraguso",
+    email: "kcruz28@illinois.edu",
+    role: "Lead Software Developer / Full Stack Developer",
+  },
+  {
+    avatar: "/AI_Loteria_24-25/home_media/dlt.png",
+    name: "Adolfo",
+    email: "SHEESH",
+    role: "Frontend Developer",
+  },
+  {
+    avatar: "/AI_Loteria_24-25/home_media/dlt.png",
+    name: "Nico",
+    email: "",
+    role: "Frontend Developer",
+  },
+  {
+    avatar: "/AI_Loteria_24-25/home_media/dlt.png",
+    name: "Geo Quadri",
+    email: "SHEESH",
+    role: "Debugger",
+  },
+  {
+    avatar: "/AI_Loteria_24-25/home_media/dlt.png",
+    name: "Alonso",
+    email: "SHEESH",
+    role: "UI Designer/ Contact Page Developer",
+  },
+  {
+    avatar: "/AI_Loteria_24-25/home_media/dlt.png",
+    name: "Ryan",
+    email: "SHEESH",
+    role: "UI Designer/ Contact Page Developer",
+  },
+];
 
-
-export default members;
+export {mechanicalTeam, programmingTeam};
--- react_frontend/src/pages/members_page.jsx
+++ react_frontend/src/pages/members_page.jsx
@@ -3,29 +3,7 @@ import Footer from "../components/footer.jsx";
 import TitleCard from "../components/title_card.jsx";
 import MemberCard from "../components/member_card.jsx";
 import "../styles/members_page_style.css";
-import members from "../data/members_data.jsx";
-
-// function MembersPage() {
-//   return (
-//     <div>
-//       <TitleCard
-//         title="Team Members"
-//         description="Meet the talented engineers behind SHPE Tech Team's innovative projects. Our diverse team brings together expertise in programming, mechanical  engineering, and more."
-//       />
-
-//       <div className="team-section">
-//         <Toggle />
-//         <h2>The Team</h2>
-//         <div className="team-row">
-//           {members.map((member, index) => (
-//             <MemberCard key={index} member={member} />
-//           ))}
-//         </div>
-//       </div>
-//       <Footer></Footer>
-//     </div>
-//   );
-// }
+import { mechanicalTeam, programmingTeam } from "../data/members_data.jsx";
 
 function MembersPage() {
   const [isToggled, setIsToggled] = useState(false);
@@ -33,6 +11,8 @@ function MembersPage() {
   const handleToggle = () => {
     setIsToggled(!isToggled);
   };
+
+  const members = isToggled ? mechanicalTeam : programmingTeam;
 
   return (
     <div>
@@ -43,18 +23,16 @@ function MembersPage() {
 
       <div className="team-section">
         <button onClick={handleToggle}>
-          {isToggled ? "Programmers" : "Mechanical"}
+          {isToggled ? "Mechanical" : "Programmers"}
         </button>
-        {isToggled && (
-          <div className="team-section">
-            <h2>The Team</h2>
-            <div className="team-row">
-              {members.map((member, index) => (
-                <MemberCard key={index} member={member} />
-              ))}
-            </div>
+        <div className="team-section">
+          <h2>The Team</h2>
+          <div className="team-row">
+            {members.map((member, index) => (
+              <MemberCard key={index} member={member} />
+            ))}
           </div>
-        )}
+        </div>
       </div>
       <Footer />
     </div>
