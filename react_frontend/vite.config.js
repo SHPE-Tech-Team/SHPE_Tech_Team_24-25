8ba0648789dfca166ab2af1c2304bef0e1341b5c
samev <samevangelista07@icloud.com>
2025-10-19 15:50:02 -0500

    Fix Deployment?

--- react_frontend/vite.config.js
+++ react_frontend/vite.config.js
@@ -1,27 +1,14 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-
+export default defineConfig(() => {
+  const isVercel = !!process.env.VERCEL; // Vercel sets this env var
   return {
     plugins: [react()],
-    base: isProduction ? "/SHPE_Tech_Team_24-25/" : "/",
-    build: {
-      outDir: "dist",
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    server: {
-      port: 3000,
-      open: true,
-      watch: {
-        usePolling: true,
-      },
-    },
+    base: isVercel ? "/" : "/SHPE_Tech_Team_24-25/",
+    build: { outDir: "dist" },
+    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
   };
 });
