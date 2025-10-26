fd65e90ca1c2f22c036ef99b0e4530903a7f8522
samev <samevangelista07@icloud.com>
2025-10-26 15:46:49 -0500

    Contact Us Page Fixes

--- react_frontend/src/pages/contact_page.jsx
+++ react_frontend/src/pages/contact_page.jsx
@@ -13,24 +13,31 @@ function ContactPage() {
 
       <div className="contact-section">
         <h2>Get in Touch</h2>
-        <div className="contact-container">
-          <h3>Our Location</h3>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3051.4105037803615!2d-88.22954292341777!3d40.11085437419841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880cd73ffb8e733b%3A0x550a408e89cd806!2sEngineering%20Hall%2C%201308%20W%20Green%20St%2C%20Urbana%2C%20IL%2061801!5e0!3m2!1sen!2sus!4v1761509553727!5m2!1sen!2sus" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-          <p>1308 W Green St Urbana, IL 61801 United States</p>
+        <div className="contact-section-container">
+          <div className="contact-container-column">
+            <div className="contact-container">
+              <h3>Follow Us</h3>
+              <p>@shpe_uiuc_tech</p>
+              <iframe  title="PSA Instagram" src="https://www.instagram.com/shpe_uiuc/embed" width="400" height="480" frameBorder="0" scrolling="no" allowtransparency="true">…</iframe>
+            </div>
+          </div>
+
+          <div className="contact-container-column">
+            <div className="contact-container">
+              <h3>Our Location</h3>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3051.4105037803615!2d-88.22954292341777!3d40.11085437419841!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880cd73ffb8e733b%3A0x550a408e89cd806!2sEngineering%20Hall%2C%201308%20W%20Green%20St%2C%20Urbana%2C%20IL%2061801!5e0!3m2!1sen!2sus!4v1761509553727!5m2!1sen!2sus" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+              <p>1308 W Green St Urbana, IL 61801 United States</p>
+            </div>
+          </div>
         </div>
 
         <div className="contact-container">
-          <h3>Email Us</h3>
-          <p>shpe-uiuc@illinois.edu</p>
-          <p>techteam@shpe-uiuc.org</p>
-        </div>
+              <h3>Email Us</h3>
+              <p>shpe-uiuc@illinois.edu</p>
+              <p>techteam@shpe-uiuc.org</p>
+            </div>
 
-        <div className="contact-container">
-          <h3>Follow Us</h3>
-          <p>@shpe_uiuc_tech</p>
-          <iframe  title="PSA Instagram" src="https://www.instagram.com/shpe_uiuc/embed" width="400" height="480" frameBorder="0" scrolling="no" allowtransparency="true">…</iframe>
-
-        </div>
+        
       </div>
       <Footer></Footer>
     </div>
